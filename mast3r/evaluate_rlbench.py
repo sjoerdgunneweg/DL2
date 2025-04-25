5decd7f9cbf8cd99c5bb23856a5453b9efa60a80
rfigge24 <rubenfigge24@gmail.com>
2025-04-25 14:13:14 +0200

    test for plotting depth

---
+++ mast3r/evaluate_rlbench.py
@@ -0,0 +1,32 @@
+from mast3r.model import AsymmetricMASt3R
+from mast3r.fast_nn import fast_reciprocal_NNs
+
+import mast3r.utils.path_to_dust3r
+from dust3r.inference import inference
+from dust3r.utils.image import load_images
+
+if __name__ == '__main__':
+    device = 'cuda'
+    schedule = 'cosine'
+    lr = 0.01
+    niter = 300
+
+    model_name = "naver/MASt3R_ViTLarge_BaseDecoder_512_catmlpdpt_metric"
+    # you can put the path to a local checkpoint in model_name if needed
+    model = AsymmetricMASt3R.from_pretrained(model_name).to(device)
+    images = load_images(['dust3r/croco/assets/Chateau1.png', 'dust3r/croco/assets/Chateau2.png'], size=512)
+    output = inference([tuple(images)], model, device, batch_size=1, verbose=False)
+
+    # at this stage, you have the raw dust3r predictions
+    view1, pred1 = output['view1'], output['pred1']
+    view2, pred2 = output['view2'], output['pred2']
+
+    pnts3d1 = pred1["pts3d"]
+    pnts3d2 = pred2["pts3d_in_other_view"]
+
+    # plot the depth of image 1 using 3d points:
+    import matplotlib.pyplot as plt
+    plt.figure()
+    plt.imshow(pnts3d1[0, :, :, 2].cpu().numpy())
+    plt.savefig('test.pdf')
+    plt.show(block=True)
