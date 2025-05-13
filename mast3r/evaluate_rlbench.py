70f29f9c491b333985e5600e29bad100bae05069
rfigge24 <rubenfigge24@gmail.com>
2025-05-13 12:56:01 +0200

    normalized depths

--- mast3r/evaluate_rlbench.py
+++ mast3r/evaluate_rlbench.py
@@ -45,9 +45,9 @@ if __name__ == '__main__':
 
     rgbimg = scene.imgs
     depths = to_numpy(scene.get_depthmaps())
+    depths_max = max([d.max() for d in depths])
+    depths = [d / depths_max for d in depths]
     print(depths[0])
-    # depths_max = max([d.max() for d in depths])
-    # depths = [d / depths_max for d in depths]
 
     imgs = []
     for i in range(len(rgbimg)):
